959a6da518c4920504b0122830c23c63fe20b1b1
Asher Mancinelli <ashermancinelli@gmail.com>
2025-04-10 09:48:28 -0700

    factorial

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+# RUN: python %s | FileCheck %s
+
 """
-http://lucacardelli.name/Papers/BasicTypechecking.pdf
-https://web.archive.org/web/20050420002559/http://www.cs.berkeley.edu/~nikitab/courses/cs263/hm.pl
-https://raw.githubusercontent.com/rob-smallshire/hindley-milner-python/refs/heads/master/inference.py
+
+Hindley Milner Type Checking
+
+The AST types are a basic DSL for constructing ASTs that the type checker understands.
+
+This is entirely based on three resources: 
+
+1. This paper by Luca Cardelli: http://lucacardelli.name/Papers/BasicTypechecking.pdf
+2. This perl script by Nikita Borisov: https://web.archive.org/web/20050420002559/http://www.cs.berkeley.edu/~nikitab/courses/cs263/hm.pl
+3. This python script by Robert Smallshire: https://raw.githubusercontent.com/rob-smallshire/hindley-milner-python/refs/heads/master/inference.py
+
+The paper contains an implementation in modula2, and the perl/python scripts are rewrites of the
+modula2 code. At times I found either script to be more useful, but I most heavily referenced the modula2
+version.
+
+The script contains FileCheck directives, so piping the output of this script to FileCheck as seen
+in the RUN line at the top of the file will test the implementation, in case you want to add more
+test cases.
+
 """
 
 import functools
@@ -65,6 +83,10 @@ class Identifier(AST):
             app = Apply(app, arg)
         return app
 
+    @staticmethod
+    def make(*args):
+        return map(Identifier, args)
+
     def __str__(self):
         return self.name
 
@@ -95,15 +117,25 @@ class BinOp(AST):
 
 class Let(AST):
     def __init__(self, name, value, body):
-        self.name, self.value, self.body = name, value, body
+        self.name = name.name if isinstance(name, Identifier) else name
+        self.value, self.body = value, body
 
     def __str__(self):
         return f"let {self.name} = {self.value} in {self.body}"
 
 
+class Letrec(AST):
+    def __init__(self, name, value, body):
+        self.name = name.name if isinstance(name, Identifier) else name
+        self.value, self.body = value, body
+
+    def __str__(self):
+        return f"(letrec {self.name} = {self.value} in {self.body})"
+
 class Lambda(AST):
     def __init__(self, param, body):
-        self.param, self.body = param, body
+        self.param = param.name if isinstance(param, Identifier) else param
+        self.body = body
 
     def __str__(self):
         return f"(λ{self.param} . {self.body})"
@@ -229,6 +261,13 @@ class TypeCheck:
                 value_type = self.infer(value, env, concrete_types)
                 body_type = self.infer(body, env.copy() | {name: value_type}, concrete_types.copy())
                 return body_type
+            case Letrec(name=name, value=value, body=body):
+                new_type = TypeVariable()
+                E = env.copy() | {name: new_type}
+                C = concrete_types.copy() | {new_type}
+                value_type = self.infer(value, E, C)
+                self.unify_type_expressions(new_type, value_type)
+                return self.infer(body, E, C)
             case _:
                 assert False
 
@@ -331,7 +370,7 @@ class TypeCheck:
                         log(n1, ts1)
                         log(n2, ts2)
                         if n1 != n2 or len(ts1) != len(ts2):
-                            raise TypeCheckError(f"Type mismatch: {expr1} {expr2}")
+                            raise TypeCheckError(f"Could not unify types: {expr1} {expr2}")
                         for subtype1, subtype2 in zip(ts1, ts2):
                             self.unify_type_expressions(subtype1, subtype2)
             case _:
@@ -351,21 +390,26 @@ def test(base_env, ast: AST | list[AST]):
         case _:
             print()
             tc = TypeCheck()
-            print('Untyped source:\n', ast)
+            print('Untyped source:', ast)
             try:
                 result = tc.infer(ast, base_env, set())
-                print('Typed result:\n', result)
+                print('Typed result:', result)
             except TypeCheckError as e:
-                print('Error:\n', e)
+                print('Error:', e)
 
 
 if __name__ == "__main__":
     pair_type = TypeOperator("*", *typevars(2))
+    t1 = TypeVariable()
     environment = {
         "true": BoolType,
         "false": BoolType,
         "*": Function(IntType, Function(IntType, IntType)),
+        "-": Function(IntType, Function(IntType, IntType)),
         "pair": Function(pair_type[0], Function(pair_type[1], pair_type)),
+        "ite": Function(BoolType, Function(t1, Function(t1, t1))),
+        "is_zero": Function(IntType, BoolType),
+        "decrement": Function(IntType, IntType),
     }
 
     for length in range(3, 10):
@@ -373,22 +417,46 @@ if __name__ == "__main__":
         ty = TypeOperator("*", *typevars(length))
         environment[name] = Function.make(ty.types, ty)
 
-    x, y, z = Identifier("x"), Identifier("y"), Identifier("z")
-    pair = Identifier("pair")
-    tuple3, tuple4, tuple5, tuple6, tuple7, tuple8, tuple9 = Identifier("tuple3"), Identifier("tuple4"), Identifier("tuple5"), Identifier("tuple6"), Identifier("tuple7"), Identifier("tuple8"), Identifier("tuple9")
-    lit5 = IntLit(5)
-    lit3 = IntLit(3)
-    true = BoolLit(True)
-    false = BoolLit(False)
+    times, minus, ite, pair, is_zero, decrement = Identifier.make("*", "-", "ite", "pair", "is_zero", "decrement")
+    x, y, z = Identifier.make("x", "y", "z")
+    tuple3, tuple4, tuple5, tuple6, tuple7, tuple8, tuple9 = [Identifier(f'tuple{i}') for i in range(3, 10)]
+    lit1, lit2, lit3, lit4, lit5 = IntLit(1), IntLit(2), IntLit(3), IntLit(4), IntLit(5)
+    true, false = BoolLit(True), BoolLit(False)
+    f, g, h, m, n = Identifier.make("f", "g", "h", "m", "n")
 
     tests = [
-        # Should fail:
-        # fn x => (pair ((x 3) ((x true)))
         Lambda(x.name, pair(x(lit5), x(true))),
-        # Should pass
+        # CHECK: Error: Could not unify types: bool int
+
         Lambda(x.name, pair(x(lit5), x(lit3))),
+        # CHECK: Typed result:
+        # CHECK-SAME: ((int -> [[T1:T[0-9]+]])
+        # CHECK-SAME: -> ([[T1]] * [[T1]]))
+
         Lambda(x.name, pair(x(true), x(false))),
-        # Should pass
-        Lambda(x.name, Lambda(y.name, tuple3(x(lit5), x(lit3), y(true)))),
+        # CHECK: Typed result:
+        # CHECK-SAME: ((bool -> [[T1:T[0-9]+]])
+        # CHECK-SAME: -> ([[T1]] * [[T1]]))
+
+        Lambda(x, Lambda(y, tuple3(x(lit5), x(lit3), y(true)))),
+        # CHECK: Typed result:
+        # CHECK-SAME: ((int -> [[A:T[0-9]+]])
+        # CHECK-SAME: -> ((bool -> [[B:T[0-9]+]])
+        # CHECK-SAME: -> ([[A]] * [[A]] * [[B]])))
+
+        Lambda(f, Lambda(g, Lambda(h, f(g(h))))),
+        # Transitivity/Function Composition
+        # CHECK: Typed result:
+        # CHECK-SAME: (([[B:T[0-9]+]] -> [[C:T[0-9]+]])
+        # CHECK-SAME: -> (([[A:T[0-9]+]] -> [[B]]) -> ([[A]] -> [[C]])))
+
+        Let(f, Lambda(g, g), pair(f(lit5), f(true))),
+        # CHECK: Typed result:
+        # CHECK-SAME: (int * bool)
+    
+        # Factorial
+        Letrec(f, Lambda(n, ite(is_zero(n), lit1, times(n, f(minus(n, lit1))))), f(lit5)),
+        # CHECK: Typed result:
+        # CHECK-SAME: int
     ]
-    test(environment, tests[-1])
+    test(environment, tests)
